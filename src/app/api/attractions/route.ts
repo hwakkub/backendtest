547d791271d578e87f5d2559fea19347fd426c7d
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-20 23:53:12 +0700

    showdataonweb

--- src/app/api/attractions/route.ts
+++ src/app/api/attractions/route.ts
@@ -21,3 +21,15 @@ export const getData = () => {
         });
     });
 };
+
+
+export async function GET(req: Request, res: Response) {
+    try {
+        const products = await getData();
+        console.log('products :', products);
+        return Response.json({ message: 'success', data: products });
+    } catch (error) {
+        console.log('error', error);
+        return Response.json({ message: 'error', data: error });
+    }
+}
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,10 @@
 import mysql from 'mysql2';
-// import * as dotenv from 'dotenv';
-import { config } from 'dotenv';
-// dotenv.config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : `.env` });
+import * as dotenv from 'dotenv';
+dotenv.config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : `.env` });
 
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
